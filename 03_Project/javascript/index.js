48deaaa5ab19f215cdffdb94d8e0030d26fed278
moon_serface <simsim916@naver.com>
2023-11-17 09:25:25 +0900

    moon

---
+++ 03_Project/javascript/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let nav = document.querySelector("nav");
+
+console.log(nav);
+nav.style.backgroundColor="##2b2b37ff";
+// if (parseInt(window.scrollY)) {
+//     nav[0].style.backgroundColor="#000000";
+// }
