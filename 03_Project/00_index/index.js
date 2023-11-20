1aba750a220265a7ac21eae7591d5778abeadd3a
moon_serface <simsim916@naver.com>
2023-11-20 16:39:37 +0900

    moon

--- 03_Project/00_index/index.js
+++ 03_Project/00_index/index.js
@@ -34,7 +34,7 @@ header.style.backgroundImage=`linear-gradient( #2b2b3720, #2b2b3720), url(${head
 headerCookie.setAttribute('src',`${cookie[random2]}`);
 
 window.addEventListener('scroll',()=>{
-    if (parseInt(window.scrollY)) {
+    if (parseInt(window.scrollY) || navBg.style.visibility == "initial") {
         nav.style.backgroundColor="#2b2b37ff";
     } else {
         nav.style.backgroundColor="#2b2b3750";
@@ -48,7 +48,7 @@ nav.addEventListener('mouseenter',()=>{
 navBg.addEventListener('mouseout',()=>{
     navBg.style.visibility="hidden";
     for (let a of subLi) a.style.visibility="hidden";
-    if (parseInt(window.scrollY)) {
+    if (parseInt(window.scrollY) ) {
         nav.style.backgroundColor="#2b2b37ff";
     } else {
         nav.style.backgroundColor="#2b2b3750";
