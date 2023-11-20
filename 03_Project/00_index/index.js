9d8eb3ec570d5acd07e13e13b5fee99e17e634dd
moon_serface <simsim916@naver.com>
2023-11-20 16:59:23 +0900

    moon

--- 03_Project/00_index/index.js
+++ 03_Project/00_index/index.js
@@ -30,7 +30,7 @@ const cookie = [
 
 let random = Math.floor(Math.random()*headerImg.length);
 let random2 = Math.floor(Math.random()*headerImg.length);
-header.style.backgroundImage=`linear-gradient( #2b2b3720, #2b2b3720), url(${headerImg[random]})`;
+header.style.backgroundImage=`url(${headerImg[random]})`;
 headerCookie.setAttribute('src',`${cookie[random2]}`);
 
 window.addEventListener('scroll',()=>{
