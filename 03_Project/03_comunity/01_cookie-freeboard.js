0ec39d2f7eb87876c8fad0f8f2477aaf6b3caaaa
User -2023HTGOB\Administrator <jiyy825@gmail.com>
2023-11-21 18:03:40 +0900

    승삼커밋

--- 03_Project/03_comunity/01_cookie-freeboard.js
+++ 03_Project/03_comunity/01_cookie-freeboard.js
@@ -1,170 +1,171 @@
 
 
 
-let noticeAR = [
-    {
-        title : '알려진 이슈를 알려드립니다.',
-        date : '2023.11.16'
-    },
-    {
-        title : '알려진 이슈를 알려드립니다.',
-        date : '2023.11.16'
-    }   
-];
 
-let freeboardAR = [
-    {
-        subject : '아오 밸런드 패치 언제함?',
-        userInfo : '금광동 쏘스윗',
-        heart : 0,
-        read : 57,
-        freeboard_date : '1시간 전',
-        content : [
-            '공감합니다.',
-            'ㄹㅇㅋㅋ',
-            '말넘심'
-        ],
-        alticle : 
-        `게임하다 갑자기 화가 치밀올라서 글써봄
+// let noticeAR = [
+//     {
+//         title : '알려진 이슈를 알려드립니다.',
+//         date : '2023.11.16'
+//     },
+//     {
+//         title : '알려진 이슈를 알려드립니다.',
+//         date : '2023.11.16'
+//     }   
+// ];
 
-
-
-        업데이트 도데체 언제함??`
-        },
-    {
-        subject : '쿠키런 풀스펙 구매합니다..!',
-        userInfo : '그린탑코인정',
-        heart : 2,
-        read : 51,
-        freeboard_date : '2시간 전',
-        content : [
-            '공감합니다.',
-            'ㄹㅇㅋㅋ',
-            '말넘심'
-        ],
-        alticle : 
-        `게임하다 갑자기 화가 치밀올라서 글써봄
+// let freeboardAR = [
+//     {
+//         subject : '아오 밸런드 패치 언제함?',
+//         userInfo : '금광동 쏘스윗',
+//         heart : 0,
+//         read : 57,
+//         freeboard_date : '1시간 전',
+//         content : [
+//             '공감합니다.',
+//             'ㄹㅇㅋㅋ',
+//             '말넘심'
+//         ],
+//         alticle : 
+//         `게임하다 갑자기 화가 치밀올라서 글써봄
 
 
 
-        업데이트 도데체 언제함??`
-    },
-    {
-        subject : '두둠칫 둠칫 친추 ㄱ',
-        userInfo : '두둠칫 둠칫',
-        heart : 3,
-        read : 22,
-        freeboard_date : '5시간 전',
-        content : [
-            '공감합니다.',
-            'ㄹㅇㅋㅋ',
-            '말넘심'
-        ],
-        alticle : 
-        `게임하다 갑자기 화가 치밀올라서 글써봄
+//         업데이트 도데체 언제함??`
+//         },
+//     {
+//         subject : '쿠키런 풀스펙 구매합니다..!',
+//         userInfo : '그린탑코인정',
+//         heart : 2,
+//         read : 51,
+//         freeboard_date : '2시간 전',
+//         content : [
+//             '공감합니다.',
+//             'ㄹㅇㅋㅋ',
+//             '말넘심'
+//         ],
+//         alticle : 
+//         `게임하다 갑자기 화가 치밀올라서 글써봄
 
 
 
-        업데이트 도데체 언제함??`
-    },
-    {
-        subject : '결정석너프하면접을거임',
-        userInfo : '봉삼이',
-        heart : 0,
-        read : 76,
-        freeboard_date : '7시간 전',
-        content : [
-            '공감합니다.',
-            'ㄹㅇㅋㅋ',
-            '말넘심'
-        ],
-        alticle : 
-        `게임하다 갑자기 화가 치밀올라서 글써봄
+//         업데이트 도데체 언제함??`
+//     },
+//     {
+//         subject : '두둠칫 둠칫 친추 ㄱ',
+//         userInfo : '두둠칫 둠칫',
+//         heart : 3,
+//         read : 22,
+//         freeboard_date : '5시간 전',
+//         content : [
+//             '공감합니다.',
+//             'ㄹㅇㅋㅋ',
+//             '말넘심'
+//         ],
+//         alticle : 
+//         `게임하다 갑자기 화가 치밀올라서 글써봄
 
 
 
-        업데이트 도데체 언제함??`
-    },
-    {
-        subject : '솔직히 자바스크립트 개어려움',
-        userInfo : '그린실버타운',
-        heart : 5,
-        read : 23,
-        freeboard_date : '10시간 전',
-        content : [
-            '공감합니다.',
-            'ㄹㅇㅋㅋ',
-            '말넘심'
-        ],
-        alticle : 
-        `게임하다 갑자기 화가 치밀올라서 글써봄
+//         업데이트 도데체 언제함??`
+//     },
+//     {
+//         subject : '결정석너프하면접을거임',
+//         userInfo : '봉삼이',
+//         heart : 0,
+//         read : 76,
+//         freeboard_date : '7시간 전',
+//         content : [
+//             '공감합니다.',
+//             'ㄹㅇㅋㅋ',
+//             '말넘심'
+//         ],
+//         alticle : 
+//         `게임하다 갑자기 화가 치밀올라서 글써봄
 
 
 
-        업데이트 도데체 언제함??`
-    },
-    {
-        subject : '스토브 인증기 왜이럼?',
-        userInfo : '그래도이쁨',
-        heart : 0,
-        read : 57,
-        freeboard_date : '12시간 전'
-    },
-    {
-        subject : '스토브 인증기 왜이럼?',
-        userInfo : '그래도이쁨',
-        heart : 0,
-        read : 57,
-        freeboard_date : '12시간 전'
-    },
-    {
-        subject : '스토브 인증기 왜이럼?',
-        userInfo : '그래도이쁨',
-        heart : 0,
-        read : 57,
-        freeboard_date : '12시간 전'
-    },
-    {
-        subject : '스토브 인증기 왜이럼?',
-        userInfo : '그래도이쁨',
-        heart : 0,
-        read : 57,
-        freeboard_date : '12시간 전'
-    },
-    {
-        subject : '스토브 인증기 왜이럼?',
-        userInfo : '그래도이쁨',
-        heart : 0,
-        read : 57,
-        freeboard_date : '12시간 전'
-    },
-    {
-        subject : '스토브 인증기 왜이럼?',
-        userInfo : '그래도이쁨',
-        heart : 0,
-        read : 57,
-        freeboard_date : '12시간 전'
-    },
-    {
-        subject : '스토브 인증기 왜이럼?',
-        userInfo : '그래도이쁨',
-        heart : 0,
-        read : 57,
-        freeboard_date : '12시간 전'
-    },
-    {
-        subject : '스토브 인증기 왜이럼?',
-        userInfo : '그래도이쁨',
-        heart : 0,
-        read : 57,
-        freeboard_date : '12시간 전'
-    },
-    {
-        subject : '스토브 인증기 왜이럼?',
-        userInfo : '그래도이쁨',
-        heart : 0,
-        read : 57,
-        freeboard_date : '12시간 전'
-    },
-];
+//         업데이트 도데체 언제함??`
+//     },
+//     {
+//         subject : '솔직히 자바스크립트 개어려움',
+//         userInfo : '그린실버타운',
+//         heart : 5,
+//         read : 23,
+//         freeboard_date : '10시간 전',
+//         content : [
+//             '공감합니다.',
+//             'ㄹㅇㅋㅋ',
+//             '말넘심'
+//         ],
+//         alticle : 
+//         `게임하다 갑자기 화가 치밀올라서 글써봄
+
+
+
+//         업데이트 도데체 언제함??`
+//     },
+//     {
+//         subject : '스토브 인증기 왜이럼?',
+//         userInfo : '그래도이쁨',
+//         heart : 0,
+//         read : 57,
+//         freeboard_date : '12시간 전'
+//     },
+//     {
+//         subject : '스토브 인증기 왜이럼?',
+//         userInfo : '그래도이쁨',
+//         heart : 0,
+//         read : 57,
+//         freeboard_date : '12시간 전'
+//     },
+//     {
+//         subject : '스토브 인증기 왜이럼?',
+//         userInfo : '그래도이쁨',
+//         heart : 0,
+//         read : 57,
+//         freeboard_date : '12시간 전'
+//     },
+//     {
+//         subject : '스토브 인증기 왜이럼?',
+//         userInfo : '그래도이쁨',
+//         heart : 0,
+//         read : 57,
+//         freeboard_date : '12시간 전'
+//     },
+//     {
+//         subject : '스토브 인증기 왜이럼?',
+//         userInfo : '그래도이쁨',
+//         heart : 0,
+//         read : 57,
+//         freeboard_date : '12시간 전'
+//     },
+//     {
+//         subject : '스토브 인증기 왜이럼?',
+//         userInfo : '그래도이쁨',
+//         heart : 0,
+//         read : 57,
+//         freeboard_date : '12시간 전'
+//     },
+//     {
+//         subject : '스토브 인증기 왜이럼?',
+//         userInfo : '그래도이쁨',
+//         heart : 0,
+//         read : 57,
+//         freeboard_date : '12시간 전'
+//     },
+//     {
+//         subject : '스토브 인증기 왜이럼?',
+//         userInfo : '그래도이쁨',
+//         heart : 0,
+//         read : 57,
+//         freeboard_date : '12시간 전'
+//     },
+//     {
+//         subject : '스토브 인증기 왜이럼?',
+//         userInfo : '그래도이쁨',
+//         heart : 0,
+//         read : 57,
+//         freeboard_date : '12시간 전'
+//     },
+// ];
 
