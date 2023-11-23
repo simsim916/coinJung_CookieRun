ebd65941465b1465fe4f788b9422c8b36033829d
User -2023FBGWX\Administrator <acts290315@gmail.com>
2023-11-23 17:39:36 +0900

    dd

--- 03_Project/05_shop/01_01_cookie_product.js
+++ 03_Project/05_shop/01_01_cookie_product.js
@@ -104,6 +104,227 @@ const productAR = [
 let productAR2 = productAR;
 let itemNum = 3;
 
+let itemRightBox = document.getElementsByClassName('item_rightbox_info')
+let itemImg = document.getElementsByClassName('item_img');
+let itemImgBtn = document.getElementsByClassName('item_imgBtn');
+let basic = document.getElementById('basic');
+let bestitem = document.getElementsByClassName('best_items');
+let maxpoint = document.getElementById('maxpoint');
+let textpoint, photopoint;
+let pointDetails = document.getElementById('pointDetails');
+let cardDetails = document.getElementById('cardDetails');
+let quantityBox = document.getElementsByClassName('item_rightbox_point_quantity');
+// 포인트 관련 기능
+// {
+// // 포인트 추가 창  
+//     {
+//     pointDetails.innerHTML =`<div>
+//     <p>구매적립</p>
+//     <div>
+//     <p>└ 기본적립</p>
+//     <p></p>
+    
+//     </div>
+//     </div>
+//     <div>
+//         <p>리뷰적립</p>
+//         <div>
+//             <p>텍스트 리뷰 작성시</p>
+//             <p></p>
+//         </div>
+//         <div>
+//             <p>포토/동영상 리뷰 작성시</p>
+//             <p></p>
+//         </div>
+//     </div>
+//     <div>
+//         <div>
+//             <div>TIP. 포인트 더 받는 방법</div>
+//             <div><a href="https://nid.naver.com">최대 5% 적립, 무료 시작</a></div>
+//             <div><a href="https://nid.naver.com">네이버 현대카드로 결제 시</a></div>
+//             <div><a href="https://nid.naver.com/">네이버페이 머니로 결제 시</a></div>
+//         </div>
+//         <div>
+//             <span>최대 2,205원</span>
+//             <span>980원</span>
+//             <span>1,225원</span>
+//             <span>490원</span>
+//         </div>
+//     </div>
+//     <hr>
+//     <ul>
+//         <li>네이버 페이 포인트는 <span id="a">구매확정 시 적립</span> 됩니다.</li>
+//         <li>적립 포인트는 할인 쿠폰 적용 및 옵션 가격을 기준으로 적립되므로, 최종 적립 포인트는 <span id="a">쿠폰 사용 여부</span> 및 <span id="a">옵션 가격</span>에 따라 달라질 수 있습니다.</li>
+//         <li><span id="a">1회 주문시 구매적립은 최대 100,000원까지 적립</span> 가능합니다. (단, 무통장입금은 최대2만원까지 적립)
+//             <p color: "gray">(네이버플러스 멤버십 적립 혜택은 예외)</p></li>
+//         <li>단, <span id="a">추가적립(추가구매적립 및 재구매/등급 적립)은 일반 구매 적립 한도(최대 100,000원)와 별개로 1회 주문 기준 최대 200,000원까지 적립</span> 가능합니다. (무통장입금 또한 별개로 최대 2만원까지 적립)</li>
+//         <li>구매확정 이후 90일까지 리뷰를 최대 2번 작성할 수 있습니다. 구매확정 이후 30일까지 리뷰를 작성할 수 있고, 구매확정 이후 31일부터는 한달사용 리뷰를 작성할 수 있습니다.</li>
+//         <li>알림받기 동의 고객 리뷰 포인트는 주문 시 알림받기 동의중인 고객님께만 지급되며 리뷰 혹은 한달사용 리뷰 작성 시 1회만 지급됩니다.</li>
+//         <li>상품결제금액 기준 구매금액 3,000원 미만인 경우 작성에 대한 포인트가 <span id="a">최대 150원 적립 제외됩니다.</span></li>
+//         <li>상품주문결제 금액에서 구매혜택과 리뷰작성 적립포인트를 차감한 금액이 0원 미만일 경우 구매혜택 및 리뷰작성에 대한 포인트 적립이 제외됩니다.</li>
+//         <li>구매확정 후 30일 이내 상품 리뷰를 작성하지 않고 한달사용리뷰만 작성했을 경우, 한달사용리뷰 포인트 대신 구매확정 30일 이내 리뷰 작성 시 지급되는 포인트로 대체 지급됩니다.</li>
+//         <li>네이버플러스 멤버십 적립 혜택은 200,000원까지는 4% 적립, 200,000원 초과 3,000,000원까지는 1%로 적립되며, <span id="a">주문 상품당 최대 20,000원까지 적립</span>됩니다.</li>
+//         <li>네이버플러스 멤버십 이벤트 진행 중에는 추가 적립 혜택이 제공될 수 있으며, 자세한 적립 정책은 멤버십 이벤트 페이지와 <a link="#" alt="custumerCenter">고객센터</a>에서 확인 가능합니다.</li>
+//     </ul>
+//     `
+// }
+// }
+        
+        if(productAR[itemNum].textreview == 1) {
+            textpoint= 50;
+        } else {
+            textpoint= 0;
+        }
+
+        if(productAR[itemNum].textreview == 1) {
+            photopoint= 100;
+        } else {
+            photopoint= 0;
+        }
+
+// }
+
+//게시글 작성
+{
+    let itemBox = document.getElementsByClassName('item');
+    itemBox[0].innerHTML=`
+    <div class="item_leftbox">
+                <div class="item_img"></div>
+                <div class="item_imgBtn"></div>
+            </div>
+            <div class="item_rightbox">
+                <div class="item_rightbox_info">
+                    <div>${productAR[itemNum].title}</div>
+                    <div>${productAR[itemNum].price.toLocaleString()} 원</div>
+                </div>
+                <div class="item_rightbox_point">
+                    <div class="item_rightbox_point_title">
+                        <span>쿠키런 스토어 고객을 위한 혜택</span>
+                    </div>
+                    <div class="item_rightbox_point_max">
+                        <div>
+                            <span>최대 적립 포인트</span>
+                            <div>
+                                <span id="maxpoint">${(productAR[itemNum].price/100+textpoint+photopoint).toLocaleString()} 원</span>
+                                <i class="fa-regular fa-circle-question"></i>
+                            </div>
+                        </div>
+                        <div>
+                            <span>└ 기본적립</span> 
+                            <div id="basic">${(productAR[itemNum].price/100).toLocaleString()} 원</div>
+                        </div>
+                    </div>
+                    <div class="item_rightbox_point_price_tip">
+                        <div>
+                            <div>TIP. 포인트 더 받는 방법</div>
+                            <div><a href="https://nid.naver.com">최대 5% 적립, 무료 시작</a></div>
+                            <div><a href="https://nid.naver.com">네이버 현대카드로 결제 시</a></div>
+                            <div><a href="https://nid.naver.com/">네이버페이 머니로 결제 시</a></div>
+                        </div>
+                        <div>
+                            <span>+최대 ${(productAR[itemNum].price*0.09).toLocaleString()}원</span>
+                            <span>${(productAR[itemNum].price*0.04).toLocaleString()}원</span>
+                            <span>${(productAR[itemNum].price*0.05).toLocaleString()}원</span>
+                            <span>${(productAR[itemNum].price*0.02).toLocaleString()}원</span>
+                        </div>
+                    </div>
+                    <div class="item_rightbox_ads"></div>
+                    <div class="item_rightbox_point_card">
+                        <span>무이자 할부</span>
+                        <span>| 카드 자세히보기</span>
+                        <i class="fa-regular fa-circle-question"></i>
+                    </div>
+                    <div id="pointDetails"></div>
+                    <div id="cardDetails"></div>
+                    <div class="item_rightbox_point_transit">
+                        <p>택배배송 | 3,000원<span>&#40;주문시 결제&#41; &#183;</span> CJ 대한통운&#40;오네&#41;</p>
+                        <p>30,000원 이상 구매 시 무료&#47;제주, 도서 지역 추가 3,000원</p>
+                        <p><a href="#">배송비 절약상품 보기</a></p>
+                    </div>
+                    <div class="item_rightbox_point_quantity">
+                        <div style="border: none;">&#45;</div>
+                        <input type="text" id="quantity" name="toBuy" value=1 style="text-align: center; "/>
+                        <div style="border: none;">&#43;</div>
+                    </div>
+                    <div class="item_rightbox_point_decision">
+                        <div>
+                            <div>
+                                <span>총 상품 금액</span>
+                                <i class="fa-regular fa-circle-question"></i>
+                            </div>
+                            <div class="item_rightbox_point_decision_total">
+                                <span id="totalAmount">개</span>
+                                <span id="totalPrice">원</span>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="btn_1">
+                                <i class="fa-solid fa-circle-chevron-right"></i>
+                                <span>구매하기</span>
+                            </div>
+                            <div class="btn_2">
+                                <i class="fa-regular fa-comment-dots"></i>
+                                <span>톡톡문의</span>
+                            </div>
+                            <div class="btn_3">
+                                <i class="fa-solid fa-heart-circle-plus"></i>
+                                <span>찜하기</span>
+                            </div>
+                            <div class="btn_4">
+                                <i class="fa-solid fa-bag-shopping"></i>
+                                <span>장바구니</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    `
+// 토탈 구하기
+    let totalAmount = document.getElementById('totalAmount');
+    let totalPrice = document.getElementById('totalPrice');
+
+    totalAmount = quantityBox[0].children[2].value;
+    totalPrice = ((quantityBox[0].children[2].value)*productAR[itemNum].price).toLocaleString();
+
+    console.log(totalAmount);
+    // console.log(${totalPrice});
+
+}
+// 베스트 상품
+{
+    productARSellDown(); // 이 때, productAR2가 '정렬된 productAR'이 됨
+    for (let i =0 ; i < 4; i++){
+    bestitem[0].innerHTML += `<div><a href="#"><img src="${productAR2[i].img[0]}"><p>${productAR2[i].title}</p><p>${productAR2[i].price.toLocaleString()}</p></a></div>`;
+    }
+}
+
+// 제품 이미지 슬라이드
+{
+    itemImg[0].innerHTML = `<img src="${productAR[itemNum].img[0]}" alt="${productAR[itemNum].title}">`;
+    itemImgBtn[0].addEventListener('click',(event)=>{
+        if (event.target.tagName=='IMG'){
+            itemImg[0].children[0].src=`${event.target.getAttribute('src')}`;
+        }
+    })
+
+    for (let i = 0 ; i < productAR[itemNum].img.length; i++){
+        itemImgBtn[0].innerHTML +=`<img src="${productAR[itemNum].img[i]}" alt="${productAR[itemNum].title}">`;
+    }
+}
+ 
+// 수량 박스 작동 기능
+{
+    quantityBox[0].addEventListener('click', (event)=> {
+        if(quantityBox[0].children[1].value>0 && event.target==quantityBox[0].children[0]){
+            quantityBox[0].children[1].value-=1;
+        }
+        if(event.target==quantityBox[0].children[2]){
+            quantityBox[0].children[1].value= +quantityBox[0].children[1].value + 1;
+        }
+    })
+}
+
+
+// 판매순 내림차순
 function productARSellDown() {
     for (let i = 0 , t; i < productAR2.length-1; i++){
         for (let j = i+1 ; j < productAR2.length ; j++) {
@@ -116,54 +337,4 @@ function productARSellDown() {
         }
     }
 }
-// 게시글 작성하는거
-{
-    let itemRightBox = document.getElementsByClassName('item_rightbox_info')
-    let itemImg = document.getElementsByClassName('item_img');
-    let itemImgBtn = document.getElementsByClassName('item_imgBtn');
-    let basic = document.getElementById('basic');
-    let bestitem = document.getElementsByClassName('best_items');
-    let maxpoint = document.getElementById('maxpoint');
-    let textpoint, photopoint;
 
-    itemImg[0].innerHTML = `<img src="${productAR[itemNum].img[0]}" alt="${productAR[itemNum].title}">`;
-    
-    {if(productAR[itemNum].textreview == 1){
-        textpoint = 50;
-    } else {
-        textpoint = 0;
-    }
-
-    if(productAR[itemNum].photoreview == 1){
-        photopoint = 100;
-    } else{
-        photopoint = 0;
-    }}
-
-    for (let i = 0 ; i < productAR[itemNum].img.length; i++){
-        itemImgBtn[0].innerHTML +=`<img src="${productAR[itemNum].img[i]}" alt="${productAR[itemNum].title}">`;
-    }
-    // 베스트 상품
-    productARSellDown(); // 이 때, productAR2가 '정렬된 productAR'이 됨
-    for (let i =0 ; i < 4; i++){
-    bestitem[0].innerHTML += `<div><a href="#"><img src="${productAR2[i].img[0]}"><p>${productAR2[i].title}</p><p>${productAR2[i].price.toLocaleString()}</p></a></div>`;
-    }
-        
-    itemRightBox[0].children[0].innerText=`${productAR[itemNum].title}`;
-    itemRightBox[0].children[1].innerText=`${productAR[itemNum].price.toLocaleString()} 원`;
-    basic.innerText = `${productAR[itemNum].price/100}원`
-    maxpoint.innerText = `${(productAR[itemNum].price/100)+textpoint+photopoint}원`
-}
-
-// 제품 이미지 슬라이드
-{
-    let itemImg = document.getElementsByClassName('item_img');
-    let itemImgBtn = document.getElementsByClassName('item_imgBtn');
-
-    itemImgBtn[0].addEventListener('click',(event)=>{
-        if (event.target.tagName=='IMG'){
-            itemImg[0].children[0].src=`${event.target.getAttribute('src')}`;
-        }
-    })
-}
-    
